9f7094b8f8f0522942c0843cf59dbc1b5e02152b
meka <meka@cfd446f6-b9c6-49ad-a440-209cd04d0f1d>
2011-01-12 01:21:46 +0000

    git-svn-id: file:///home/svn/meka-ros-pkg@16 cfd446f6-b9c6-49ad-a440-209cd04d0f1d

--- trunk/mic_array/srv/lisp/mic_array/_package.lisp
+++
@@ -1,9 +0,0 @@
-(defpackage mic_array-srv
-  (:use cl
-        roslisp-msg-protocol)
-  (:export
-   "MICARRAYPARAM"
-   "<MICARRAYPARAM-REQUEST>"
-   "<MICARRAYPARAM-RESPONSE>"
-  ))
-
--- trunk/mic_array/srv/lisp/mic_array/_package_MicArrayParam.lisp
+++
@@ -1,6 +0,0 @@
-(in-package mic_array-srv)
-(export '(GAINS-VAL
-          WINDOW_TIME-VAL
-          THRESHOLD-VAL
-          SLEW_RATE-VAL
-))
