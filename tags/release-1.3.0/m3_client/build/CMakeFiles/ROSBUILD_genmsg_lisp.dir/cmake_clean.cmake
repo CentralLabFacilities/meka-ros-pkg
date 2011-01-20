89da6677e52b68bb7ed2e50927a0845e98c95d06
meka <meka@cfd446f6-b9c6-49ad-a440-209cd04d0f1d>
2011-01-20 01:34:31 +0000

    Ready for ISS base shipment.
    git-svn-id: file:///home/svn/meka-ros-pkg@27 cfd446f6-b9c6-49ad-a440-209cd04d0f1d

---
+++ tags/release-1.3.0/m3_client/build/CMakeFiles/ROSBUILD_genmsg_lisp.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ROSBUILD_genmsg_lisp"
+  "../msg/lisp/m3_client/M3BaseStatus.lisp"
+  "../msg/lisp/m3_client/_package.lisp"
+  "../msg/lisp/m3_client/_package_M3BaseStatus.lisp"
+  "../msg/lisp/m3_client/M3OmnibaseJoy.lisp"
+  "../msg/lisp/m3_client/_package.lisp"
+  "../msg/lisp/m3_client/_package_M3OmnibaseJoy.lisp"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/ROSBUILD_genmsg_lisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ tags/release-1.3.0/m3_client/build/CMakeFiles/loadx6_client_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/loadx6_client_example.dir/src/examples/loadx6_client_example.o"
+  "../bin/loadx6_client_example.pdb"
+  "../bin/loadx6_client_example"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/loadx6_client_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
