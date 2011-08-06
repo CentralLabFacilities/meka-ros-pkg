82c57e0376c06a5df211eb50d4c9cdc111cd5e56
meka <meka@cfd446f6-b9c6-49ad-a440-209cd04d0f1d>
2011-08-06 19:28:35 +0000

    Finished Simon upgrade.
    git-svn-id: file:///home/svn/meka-ros-pkg@99 cfd446f6-b9c6-49ad-a440-209cd04d0f1d

---
+++ tags/release-1.3.8/m3_client/build/CMakeFiles/hand_client_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/hand_client_example.dir/src/examples/hand_client_example.o"
+  "../bin/hand_client_example.pdb"
+  "../bin/hand_client_example"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/hand_client_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
