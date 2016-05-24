ddaa90da7e9802c1f025f964ab8b74bf089a8a0e
Leroy Ruegemer <lruegeme@techfak.uni-bielefeld.de>
2016-05-24 14:19:15 +0200

    created nodes folder for run script

--- meka_posture_execution/setup.py
+++ meka_posture_execution/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-     packages=['meka_posture_execution'],
+     packages=['meka_posture_execution', 'meka_posture_execution.interfaces'],
      package_dir={'': 'src'}
 )
 
