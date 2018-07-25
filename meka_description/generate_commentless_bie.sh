057b80b4c61dc3adeba3407f07dc26a8a7bac954
clf <clf@clf.de>
2018-07-25 17:01:56 +0200

    adding floka head

--- meka_description/generate_commentless_bie.sh
+++ meka_description/generate_commentless_bie.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-echo "generating bie.xml"
+echo "generating bie.xml and bie_floka.xml"
 cd $1/robots
 rosrun xacro xacro bie_sim.urdf.xacro > bie.urdf
 xmlstarlet ed -d '//comment()' bie.urdf > bie.xml
+
+rosrun xacro xacro bie_floka_sim.urdf.xacro > bie_floka.urdf
+xmlstarlet ed -d '//comment()' bie_floka.urdf > bie_floka.xml
+
 rm bie.urdf
+rm bie_floka.urdf
