f6103c8eba33d48f110a8cdc336cb642f73c78b4
Sebastian Meyer zu Borgsen <semeyerz@techfak.uni-bielefeld.de>
2019-09-26 19:07:18 +0200

    provide smooth data on robot motion

--- force_helper/scripts/force_helper_node.py
+++ force_helper/scripts/force_helper_node.py
@@ -2,17 +2,28 @@
 
 import rospy
 from geometry_msgs.msg import WrenchStamped
+from trajectory_msgs.msg import JointTrajectory
 from collections import deque
+from std_msgs.msg import Float32
 import numpy as np
+
+from control_msgs.msg import JointTrajectoryControllerState
 
 # init ROS
 rospy.init_node('force_helper')
-pub = rospy.Publisher('/force_helper', WrenchStamped, queue_size=1)
+pub_force = rospy.Publisher('/force_helper', WrenchStamped, queue_size=1)
+pub_arm = rospy.Publisher('/force_helper/arm', Float32, queue_size=1)
 
 # hyperparameters
-window_size = 10
+window_size = 15
 step_size = 0.01
 wrench_queue = deque(maxlen=window_size*2)
+arm_queue = deque(maxlen=window_size*2)
+current_command = None
+last_command = None
+arm_avg = 0
+last_receive_time = None
+arm_state_pos = None
 
 def _on_new_force_torque(wrench):
     global window_size
@@ -57,10 +68,50 @@ def _on_new_force_torque(wrench):
         ws.wrench.torque.x = np.linalg.norm(current_force)
         ws.wrench.torque.y = np.sum(current_force)
 
-        pub.publish(ws)
+        pub_force.publish(ws)
+
+def _on_new_arm_command(command):
+    global arm_queue
+    global current_command
+    global last_command
+    global last_receive_time
+
+    last_command = current_command
+    current_command = command.points[0]
+    last_receive_time = rospy.Time.now()
+
+    arm_queue.append(command.points[0])
 
 
+def _on_arm_timer(event):
+    global arm_queue
+    global current_command
+    global last_command
+    global arm_avg
+    global last_receive_time
+    global arm_state_pos
+
+    if current_command == None or last_command == None:
+        return
+
+    vel = np.linalg.norm(np.subtract(arm_state_pos,current_command.positions))/current_command.time_from_start.to_sec()
+    if(rospy.Time.now() > last_receive_time + current_command.time_from_start*2):
+        vel =0
+
+    n = 0.9
+    arm_avg = (n*arm_avg+(1-n)* np.clip(vel*8,0,1))
+
+    pub_arm.publish(arm_avg)
+
+def _on_new_arm_state(state):
+    global arm_state_pos
+
+    arm_state_pos = state.actual.positions
 
 # subscribers
 wrench_sub = rospy.Subscriber('/meka_ros_pub/m3loadx6_ma29_l0/wrench', WrenchStamped, _on_new_force_torque)
+arm_command_sub = rospy.Subscriber('/meka_roscontrol/right_arm_position_trajectory_controller/command', JointTrajectory, _on_new_arm_command)
+arm_sate_sub = rospy.Subscriber('/meka_roscontrol/right_arm_position_trajectory_controller/state', JointTrajectoryControllerState, _on_new_arm_state)
+
+rospy.Timer(rospy.Duration(0.03), _on_arm_timer, oneshot=False)
 rospy.spin()
